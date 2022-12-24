0d5239526cc2a4716c5a80430e27b00cf338fc5e
Isra Adel <israsalama4@gmail.com>
2022-12-24 11:15:47 +0400

    createBoard Function

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-let board_grid = document.querySelector(".board");
-board_grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board_grid.style.gridTemplateRows = 'repeat(16, 1fr';
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+   
+});
 
+function createBoard(size){
+    let board_div = document.querySelector(".board");
+    board_div.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
+    board_div.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+    let numDivs = size * size;
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor = "yellow";
+        board_div.insertAdjacentElement("beforeend", div);
+    }
+
+
+}
