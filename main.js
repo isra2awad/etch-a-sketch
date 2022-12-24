de93dfd16514e63d156e63fd5920f9ae0392fbdc
Isra Adel <israsalama4@gmail.com>
2022-12-24 10:26:59 +0400

    Modifying and Styling the page (HTML, CSS, Bootstrap)

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 let board_grid = document.querySelector(".board");
-board_grid.style.gridTemplateColumns = 'repeat(16, 1fr)'
+board_grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
+board_grid.style.gridTemplateRows = 'repeat(16, 1fr';
+
